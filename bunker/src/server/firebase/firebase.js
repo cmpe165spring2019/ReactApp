a9a078a0164f77c92a105af07231d70a6768615b
Hoa Nguyen <nguyenhoa2896@gmail.com>
2019-02-23 20:09:19 -0800

    Create Test Page for BackEnd Team

--- bunker/src/server/firebase/firebase.js
+++ bunker/src/server/firebase/firebase.js
@@ -1,17 +1,10 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import Config from './config';
 
 
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-};
-
+const config = Config;
 class Firebase {
   constructor() {
     app.initializeApp(config);
@@ -20,7 +13,7 @@ class Firebase {
     //Initialize firebase database
     this.database = app.database();
     //Initialize Google Authentication
-    this.provider = app.auth.GoogleAuthProvider();
+    this.provider = new app.auth.GoogleAuthProvider();
   }
 
   //TODO Auth API
@@ -28,7 +21,7 @@ class Firebase {
 
   //Google SignIn
   googleSignIn= () => {
-    this.auth().signInWithPopup(provider).then(result => {
+    this.auth().signInWithPopup(this.provider).then(result => {
       console.log(result);
       console.log("Google Account Linked");
     }).catch(err => {
