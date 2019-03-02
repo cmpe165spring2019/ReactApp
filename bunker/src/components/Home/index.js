8e27ce3a9a39f7554cd2ffa0bf7ed5ac1d089323
Hoa Nguyen <nguyenhoa2896@gmail.com>
2019-03-01 16:07:02 -0800

    delete message

--- bunker/src/components/Home/index.js
+++ bunker/src/components/Home/index.js
@@ -3,40 +3,12 @@ import { compose } from 'recompose';
 
 import { withAuthorization, withEmailVerification } from '../Session';
 import { withFirebase } from '../Firebase';
-import Messages from '../Messages';
 
-class HomePage extends Component {
-  constructor(props) {
-    super(props);
+const HomePage = () => 
+  (<div>
 
-    this.state = {
-      users: null,
-    };
-  }
-
-  componentDidMount() {
-    this.props.firebase.users().on('value', snapshot => {
-      this.setState({
-        users: snapshot.val(),
-      });
-    });
-  }
-
-  componentWillUnmount() {
-    this.props.firebase.users().off();
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Home Page</h1>
-        <p>The Home Page is accessible by every signed in user.</p>
-
-        <Messages users={this.state.users} />
-      </div>
-    );
-  }
-}
+    </div>
+  )
 
 const condition = authUser => !!authUser;
 
