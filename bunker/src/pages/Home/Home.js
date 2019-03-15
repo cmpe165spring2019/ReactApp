8558a449958bed4bb72d37512d951c3a3dcb6321
Vivian Leung <vivz753@gmail.com>
2019-03-15 15:20:02 -0700

    use ' instead of "

--- bunker/src/pages/Home/Home.js
+++ bunker/src/pages/Home/Home.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { compose } from "recompose";
+import React, { Component } from 'react';
+import { compose } from 'recompose';
 
-import { withAuthorization, withEmailVerification } from "../../server/Session";
-import { withFirebase } from "../../server/Firebase";
+import { withAuthorization, withEmailVerification } from '../../server/Session';
+import { withFirebase } from '../../server/Firebase';
 
 import {
   Button,
@@ -13,24 +13,24 @@ import {
   Message,
   Card,
   Divider
-} from "semantic-ui-react";
+} from 'semantic-ui-react';
 
 const condition = authUser => !!authUser;
 
 const HomePage = () => (
   <div>
-    <h3 class="ui header">Header</h3>
-    <div class="ui section divider" />
-    <div class="ui segment">
-      <div class="ui two column very relaxed grid">
-        <div class="column">
+    <h3 class='ui header'>Header</h3>
+    <div class='ui section divider' />
+    <div class='ui segment'>
+      <div class='ui two column very relaxed grid'>
+        <div class='column'>
           <ListingBase />
         </div>
-        <div class="column">
+        <div class='column'>
           <p>map</p>
         </div>
       </div>
-      <div class="ui vertical divider" />
+      <div class='ui vertical divider' />
     </div>
   </div>
 );
@@ -43,23 +43,23 @@ class ListingBase extends Component {
   render() {
     return (
       <div>
-        <div class="ui card">
-          <div class="image">
-            <img src="/images/avatar2/large/kristy.png" />
+        <div class='ui card'>
+          <div class='image'>
+            <img src='/images/avatar2/large/kristy.png' />
           </div>
-          <div class="content">
-            <a class="header">Master Bedroom SF</a>
-            <div class="meta">
-              <span class="date">$150 per night + $50 cleaning fee</span>
+          <div class='content'>
+            <a class='header'>Master Bedroom SF</a>
+            <div class='meta'>
+              <span class='date'>$150 per night + $50 cleaning fee</span>
             </div>
-            <div class="description">
+            <div class='description'>
               Walking distance from cal train and union square.
             </div>
           </div>
 
-          <div class="extra">
+          <div class='extra'>
             Rating:
-            <div class="ui star rating" data-rating="2" />
+            <div class='ui star rating' data-rating='2' />
           </div>
         </div>
       </div>
