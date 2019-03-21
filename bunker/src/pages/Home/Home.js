8c289c6a54cd4c941552bdbcb93f04b802692cf8
ZiqiYuanAngela <yuanziqi93@gmail.com>
2019-03-21 12:54:25 -0700

    enhanced calenar

--- bunker/src/pages/Home/Home.js
+++ bunker/src/pages/Home/Home.js
@@ -4,8 +4,9 @@ import { withRouter } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import { withAuthorization, withEmailVerification } from '../../server/Session';
 import { withFirebase } from '../../server/Firebase';
-import Logo from './linkedin_banner_image_11.pdf';
+import Logo from './linkedin_banner_image_1.png';
 import{DateInput}from 'semantic-ui-calendar-react';
+import * as moment from 'moment';
 import {
   Button,
   Form,
@@ -44,9 +45,9 @@ let PriceR=[];
 var i=0;
 while(i<700){
   let obj={
-    key:i,
-    text:i,
-    value:i
+    key:i+'--'+(i+100),
+    text:i+'--'+(i+100),
+    value:i+'--'+(i+100)
   };i=i+100;
   PriceR.push(obj);
 }
@@ -74,39 +75,74 @@ const HomePage = () => (
 
 const condition = authUser => !!authUser;
 
-const INITIAL_STATE = {
-  email: '',
-  password: '',
-  error: null,
-};
+
+const today=moment().format('MM-DD-YYYY');
+const tommorrow=moment().add(1,'days').format('MM-DD-YYYY');
+//const date3=moment().add(180,'days').format('MM-DD-YYYY');
+//const date4=moment().add(120,'days').format('MM-DD-YYYY');
+//const date5=moment().add(300,'days').format('MM-DD-YYYY');
+
 
 
 class SearchFilterBar extends Component {
   constructor(props) {
     super(props);
-      //this.state = { ...INITIAL_STATE };
       this.state={
         dateIn:'',
         dateOut:'',
-        todayString: ''
+        maxCheckIn: '',
+        minCheckout:tommorrow,
       };
   }
 
 
 
-  handleChange=(event,{name,value})=>{
+  handleCheckInDate=(event,{name,value})=>{
+
+    let parts=value.split("-");
+    let dt=new Date(parseInt(parts[2]),parseInt(parts[0]-1),parseInt(parts[1]));
+    //if(value.length>=10 && dt<new Date()){
+    //  window.alert("The Earliest CheckInDate is today, please choose from calendar")
+  //  }
+  //  else{
+
+    let date=moment(dt).add(1,'days').format('MM-DD-YYYY');
+    console.log(date);
+    //let date1=moment(dt2).add(120,'days').format('MM-DD-YYYY');
     if(this.state.hasOwnProperty(name)){
-      this.setState({[name]:value});
+      console.log("good1")
+      this.setState({[name]:value,minCheckout:date});
     }
+  //}
   }
 
+  handleCheckOutDate=(event,{name,value})=>{
+        console.log("good2");
+        let parts=value.split("-");
+        let dt=new Date(parseInt(parts[2]),parseInt(parts[0]-1),parseInt(parts[1]));
+      //  if(value.length>=10 && dt<=new Date()){
+        //  window.alert("The Earliest CheckOutDate is tommorrow, please choose from calendar");
+      //  }
+      //  else{
+        let date=moment(dt).subtract(1,'days').format('MM-DD-YYYY');
+        //let date1=today;
+      //  if(moment(dt).subtract(120,'days')>moment()){
+        //date1=moment(dt).subtract(120,'days').format('MM-DD-YYYY');
+    //  }
+    if(this.state.hasOwnProperty(name)){
+      console.log("good3");
+      this.setState({[name]:value,maxCheckIn:date});
+    }
+  //}
 
+  }
 
-  render() {
+render() {
 
     const { email, password, error } = this.state;
 
     const isInvalid = password === '' || email === '';
+
 
     return (
 
@@ -120,26 +156,27 @@ class SearchFilterBar extends Component {
 
               <Grid>
                 <Grid.Row></Grid.Row>
-                <Grid.Column width ={3}>
-                <div>CheckInDate</div><DateInput  name="dateIn" minDate="03-16-2019" dateFormat="MM-DD-YYYY" onChange={this.handleChange} value={this.state.dateIn} icon="bullhorn" iconPosition="left" placeholder="MM/DD/YYYY"/>
+                <Grid.Row columns={5}>
+                <Grid.Column>
+                <div>CheckInDate</div><DateInput name="dateIn"  minDate={today} maxDate={this.state.maxCheckIn} dateFormat="MM-DD-YYYY" onChange={this.handleCheckInDate} value={this.state.dateIn} icon="bullhorn" iconPosition="left" placeholder="MM-DD-YYYY"/>
                   </Grid.Column>
+                    <Grid.Column>
 
-                    <Grid.Column width ={3}>
-                <div>CheckOutDate</div><DateInput name="dateOut" minDate="03-17-2019" dateFormat="MM-DD-YYYY" onChange={this.handleChange} value={this.state.dateOut} icon="paper plane" iconPosition="left" placeholder="MM/DD/YYYY"/>
+                <div>CheckOutDate</div><DateInput name="dateOut"  minDate={this.state.minCheckout} dateFormat="MM-DD-YYYY" onChange={this.handleCheckOutDate} value={this.state.dateOut} icon="paper plane" iconPosition="left" placeholder="MM-DD-YYYY"/>
                 </Grid.Column>
 
-                   <Grid.Column width={3}>
+                   <Grid.Column>
                    <div>Number of Guests</div><GuestNum />
                    </Grid.Column>
 
-                   <Grid.Column width={3}>
+                   <Grid.Column>
                    <div>Star Level</div><Stars /></Grid.Column>
 
-                    <Grid.Column width={3}>
-                    <div>Lowest Price</div><PriceRange />
+                    <Grid.Column>
+                    <div>Price Range</div><PriceRange />
                     </Grid.Column>
 
-
+                </Grid.Row>
               </Grid>
 
           </Grid.Column>
