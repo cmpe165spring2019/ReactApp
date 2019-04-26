1d147a4c24b6dde7ada507b233a5a23794df6d22
gabrielbaki <gabrielbaky@gmail.com>
2019-04-26 12:34:59 -0700

    maps pins

---
+++ bunker/src/pages/Home/components/Maps.js
@@ -0,0 +1,62 @@
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import React, { Component } from "react";
+import * as ROUTES from "../../../constants/routes";
+
+// Backend functionalities
+import { withFirebase } from "../../../server/Firebase/index";
+
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onMapClicked = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+  //loop this marker
+  render() {
+    return (
+      <Map google={this.props.google} onClick={this.onMapClicked}>
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"San Francisco"}
+          position={{ lat: 37.759703, lng: -122.428093 }}
+        />
+
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"China Town"}
+          position={{ lat: 38.759703, lng: -122.428093 }}
+        />
+
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyBEBj6JuJ5LgehsCQtT3cF2d3Qloo84KC0"
+})(MapContainer);
--- bunker/src/pages/Hotel/Hotel.js
+++ bunker/src/pages/Hotel/Hotel.js
@@ -1,187 +1,192 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Maps from "../Home/components/Maps";
 
 // Components
-import { withFirebase } from '../../server/Firebase';
+import { withFirebase } from "../../server/Firebase";
 import {
-    Grid,
-    Button,
-    Container,
-    Header,
-    Segment,
-    Divider,
-    Rating
-} from 'semantic-ui-react';
-
+  Grid,
+  Button,
+  Container,
+  Header,
+  Segment,
+  Divider,
+  Rating,
+  Card,
+  Placeholder
+} from "semantic-ui-react";
 
 //Debugging purposes
-import * as util from 'util' // has no default export
-import CheckInOutCalendar from '../../commonComponents/CheckInOutCalendar';
+import * as util from "util"; // has no default export
+import CheckInOutCalendar from "../../commonComponents/CheckInOutCalendar";
 import RoomTypeSelect from "../../commonComponents/RoomTypeSelect";
-import RoomQuantitySelect from '../../commonComponents/Navigation/RoomQuantitySelect';
-
+import RoomQuantitySelect from "../../commonComponents/Navigation/RoomQuantitySelect";
 
 class HotelPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...props.location.state
+    };
+  }
 
-    constructor(props) {
-        super (props);
-        this.state = {
-            ...props.location.state,
+  componentDidMount() {
+    //parse dates into check in and out
+    this.parseDatesRange(this.state.datesRange);
+    this.calculateRoomPrice();
+  }
 
-        }
+  componentDidUpdate() {
+    console.log(util.inspect(this.state));
+  }
+
+  parseDatesRange = datesRange => {
+    if (datesRange.length > 13) {
+      let parsedValue = datesRange.split(" ");
+      let checkInString = parsedValue[0];
+      let checkOutString = parsedValue[2];
+      let checkInArray = checkInString.split("-");
+      let checkOutArray = checkOutString.split("-");
+      let checkInDate = new Date(
+        parseInt(checkInArray[2]),
+        parseInt(checkInArray[0] - 1),
+        parseInt(checkInArray[1])
+      );
+      let checkOutDate = new Date(
+        parseInt(checkOutArray[2]),
+        parseInt(checkOutArray[0] - 1),
+        parseInt(checkOutArray[1])
+      );
+      this.setState({
+        ...this.state,
+        checkInDate: checkInDate,
+        checkOutDate: checkOutDate
+      });
+    }
+  };
+
+  handleCheckInOut = (event, { name, value }) => {
+    //set datesRange whenever calendar range is updated
+    if (this.state.hasOwnProperty(name)) {
+      this.setState({ [name]: value });
     }
 
-    componentDidMount(){
-        //parse dates into check in and out
-        this.parseDatesRange(this.state.datesRange);
+    //parse the dates into checkInDate and checkOutDate as Date objects after the user clicks the 2nd date
+    this.parseDatesRange(value);
+  };
+
+  handleRoomTypeQuantity = (e, { name, value }) => {
+    this.setState(
+      {
+        [name]: value
+      },
+      () => {
         this.calculateRoomPrice();
-    }
+      }
+    );
+  };
 
-    componentDidUpdate () {
-        console.log(util.inspect(this.state));
-    }
+  calculateRoomPrice() {
+    const { room_types } = this.state.hotel.data;
+    const { roomQuantity } = this.state;
+    console.log("room_types: " + util.inspect(room_types));
+    const roomTypeData = room_types.filter(
+      roomType => roomType.type === this.state.roomType
+    );
+    const roomPrice = roomTypeData[0].price;
 
-    parseDatesRange = (datesRange) => {
-        if(datesRange.length > 13){
-            let parsedValue = datesRange.split(" ");
-            let checkInString = parsedValue[0];
-            let checkOutString = parsedValue[2];
-            let checkInArray = checkInString.split("-");
-            let checkOutArray = checkOutString.split("-");
-            let checkInDate = new Date(
-                parseInt(checkInArray[2]),
-                parseInt(checkInArray[0]-1),
-                parseInt(checkInArray[1])
-            );
-            let checkOutDate = new Date(
-                parseInt(checkOutArray[2]),
-                parseInt(checkOutArray[0]-1),
-                parseInt(checkOutArray[1])
-            );
-            this.setState({
-                ...this.state,
-                checkInDate: checkInDate,
-                checkOutDate: checkOutDate,
-            });
+    const pricePerNight = roomPrice * roomQuantity;
+    console.log(pricePerNight);
 
-    }
-}
+    this.setState({
+      hotel: {
+        ...this.state.hotel,
+        data: {
+          ...this.state.hotel.data,
+          currentRoomPrice: roomPrice
+        }
+      },
+      pricePerNight: pricePerNight
+    });
+  }
 
-    handleCheckInOut=(event,{name,value})=>{
-        //set datesRange whenever calendar range is updated
-            if(this.state.hasOwnProperty(name)){
-                this.setState({[name]:value});
-            }
-        
-        //parse the dates into checkInDate and checkOutDate as Date objects after the user clicks the 2nd date
-        this.parseDatesRange(value);    
-    }
+  render() {
+    const {
+      name,
+      address,
+      details,
+      image,
+      rating,
+      room_types
+    } = this.state.hotel.data;
+    const { datesRange, roomType, roomQuantity, pricePerNight } = this.state;
+    console.log("roomQuantity: " + roomQuantity);
 
-    handleRoomTypeQuantity=(e, {name, value})=>{
-        this.setState({
-            [name]: value
-        },
-        () => {
-            this.calculateRoomPrice();
-        });
-    }
+    return (
+      <Grid centered celled columns={2}>
+        <Grid.Row>insert carousel here Images</Grid.Row>
+        <Grid.Row width={13} centered columns={3}>
+          <Grid.Column width={8}>
+            <Segment textAlign="left" padded="very">
+              <Container textAlign="left">
+                <Header as="h2">{name}</Header>
+                <p>
+                  {address.street}
+                  <br />
+                  {address.city}, {address.state} {address.country}
+                </p>
+                <p>{details}</p>
+              </Container>
+            </Segment>
+            <Card.Content>
+              <Placeholder style={{ height: 100, width: 100 }}>
+                <Maps />
+              </Placeholder>
+            </Card.Content>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <Segment padded="very">
+              <Container textAlign="center">
+                <Header as="h3">${pricePerNight} / night</Header>
+                <Rating
+                  disabled
+                  icon="star"
+                  defaultRating={rating}
+                  maxRating={5}
+                />
+                <br />
+                <Divider />
+                <br />
+                <p>
+                  Check In/Out Date:
+                  <CheckInOutCalendar
+                    onChange={this.handleCheckInOut.bind(this)}
+                    value={datesRange}
+                  />
+                </p>
+                <p>
+                  Room Type/Quantity:
+                  <br />
+                  <RoomTypeSelect
+                    onChange={this.handleRoomTypeQuantity.bind(this)}
+                    defaultValue={roomType}
+                  />
+                  <RoomQuantitySelect
+                    onChange={this.handleRoomTypeQuantity.bind(this)}
+                    defaultValue={roomQuantity}
+                  />
+                </p>
+                <br />
+                <Divider />
+                <br />
 
-    calculateRoomPrice () {
-        const { room_types } = this.state.hotel.data;
-        const { roomQuantity } = this.state;
-        console.log('room_types: ' + util.inspect(room_types));
-        const roomTypeData = room_types.filter(roomType=> roomType.type === this.state.roomType);
-        const roomPrice = roomTypeData[0].price;
-
-        const pricePerNight = roomPrice*roomQuantity;
-        console.log(pricePerNight);
-
-        this.setState({
-            hotel : {
-                ...this.state.hotel,
-                data: {
-                    ...this.state.hotel.data,
-                    currentRoomPrice: roomPrice
-                }
-            },
-            pricePerNight: pricePerNight
-        })
-    }
-          
-
-    render () {
-        const { name, address, details, image, rating, room_types } = this.state.hotel.data;
-        const { datesRange, roomType, roomQuantity, pricePerNight } = this.state;
-        console.log('roomQuantity: ' + roomQuantity);
-
-        return (
-            <Grid centered celled columns={2}>
-                <Grid.Row>
-                    insert carousel here
-                    Images
-                </Grid.Row>
-                <Grid.Row width={13} centered columns={3}>
-                    <Grid.Column width={8}>
-                    <Segment textAlign='left' padded='very'>
-                        <Container textAlign='left'>
-                            <Header as='h2'>
-                            {name}
-                            </Header>
-                            <p>
-                                {address.street}
-                                <br></br>
-                                {address.city}, {address.state} {address.country}
-                            </p>
-                            <p>
-                                {details}
-                            </p>
-                        </Container>
-                    </Segment>
-                    </Grid.Column>
-                    <Grid.Column width={4}>
-                    <Segment padded='very'>
-                        <Container textAlign='center'>
-                            <Header as='h3'>
-                            ${pricePerNight} / night
-                            </Header>
-                            <Rating disabled icon='star' defaultRating={rating} maxRating={5} />
-                            <br></br>
-                           <Divider/>
-                           <br></br>
-                            <p>
-                            Check In/Out Date:
-                            <CheckInOutCalendar
-                            onChange={this.handleCheckInOut.bind(this)}
-                            value={datesRange}
-                            />
-                            </p>
-                            <p>
-                            Room Type/Quantity:   
-                            <br></br>                           
-                            <RoomTypeSelect
-                                onChange={this.handleRoomTypeQuantity.bind(this)}
-                                defaultValue={roomType}
-                            />
-                            <RoomQuantitySelect
-                                onChange={this.handleRoomTypeQuantity.bind(this)}
-                                defaultValue={roomQuantity}
-                            />
-                           </p>
-                           <br></br>
-                           <Divider/>
-                           <br></br>
-
-                            <Button fluid>
-                                Book
-                            </Button>
-                        </Container>
-                    </Segment>
-                    </Grid.Column>
-
-                </Grid.Row>
-            </Grid>
-
-        )
-    }
+                <Button fluid>Book</Button>
+              </Container>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
 }
 
 export default HotelPage;
