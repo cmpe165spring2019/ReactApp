fc59a13ef59ab48af29ac1ef773e9cb0e49da877
Hoa Nguyen <nguyenhoa2896@gmail.com>
2019-05-07 16:59:01 -0700

    add condition for null user

--- bunker/src/pages/Hotel/CheckOut/CheckOut.js
+++ bunker/src/pages/Hotel/CheckOut/CheckOut.js
@@ -6,15 +6,17 @@ import PayPalButton from "../../../server/Payment/PayPalButton";
 import {withAuthentication} from "../../../server/Session";
 
 const CheckOut = props => {
-
 	const [isError, setIsError] = React.useState(false);
 	const [isUseReward, setIsUseReward] = React.useState(false);
-	const [error, setError] = React.useState(new Error('null'));
+	const [error, setError] = React.useState(new Error("null"));
 	const [isSuccess, setIsSuccess] = React.useState(false);
 	const [user, setUser] = React.useState({});
-	React.useEffect( () => {
-		setUser(props.authUser)
-	}, [props.authUser]);
+	React.useEffect(
+		() => {
+			setUser(props.authUser);
+		},
+		[props.authUser]
+	);
 
 	const handleUseReward = () => {
 		setIsUseReward(!isUseReward);
@@ -23,13 +25,19 @@ const CheckOut = props => {
 
 	const onSuccess = payment => {
 		console.log("Successful payment!", payment);
-		const reservation_with_payment = {user_id: user.uid, payment: payment, hotel_id: hotel.id, datesRange, ...reservation, };
+		const reservation_with_payment = {
+			user_id: user.uid,
+			payment: payment,
+			hotel_id: hotel.id,
+			datesRange,
+			...reservation
+		};
 		console.log(reservation_with_payment);
 		props.firebase.addReservationToDB(
 			user.uid,
 			reservation_with_payment,
 			isUseReward,
-			user.reward_points,
+			user.reward_points
 		);
 		setIsSuccess(payment.paid);
 		setIsError(false);
@@ -50,7 +58,7 @@ const CheckOut = props => {
 		<Modal
 			centered={true}
 			size="small"
-            role="dialog"
+			role="dialog"
 			trigger={
 				<Button
 					color="blue"
@@ -69,61 +77,71 @@ const CheckOut = props => {
 					<CheckOutForm
 						hotel={hotel}
 						reservation={reservation}
-						user={user}
-						reward_points={user.reward_points}
+						user={user || {}}
 						isUseReward={isUseReward}
 					/>
 				</Modal.Description>
 			</Modal.Content>
 			<Modal.Actions>
-				<Segment>
-					<Grid>
-						<Grid.Row columns='equal'>
-						<Grid.Column>
-						<Button floated='left'
-					content={
-						isUseReward
-							? "Reward is used"
-							: `Reward left: ${user.reward_points}`
-					}
-					disabled={user.reward_points <= 0}
-					negative={isUseReward}
-					positive={!isUseReward}
-					onClick={handleUseReward}
-				/>
-							</Grid.Column>
-							<Grid.Column >
-						{user.uid ? (<PayPalButton
-					new_end={reservation.end_date}
-					new_start={reservation.start_date}
-					user_id={user.uid}
-					total={reservation.price || 100}
-					currency={"USD"}
-					commit={true}
-					onSuccess={onSuccess}
-					onError={onError}
-					onCancel={onCancel}
-				/>) : <div/>}
-						</Grid.Column>
-
-						</Grid.Row>
-					</Grid>
-
-				</Segment>
+				{user ? (
+					<Segment>
+						<Grid>
+							<Grid.Row columns="equal">
+								<Grid.Column>
+									<Button
+										floated="left"
+										content={
+											isUseReward
+												? "Reward is used"
+												: `Reward left: ${user.reward_points}`
+										}
+										disabled={user.reward_points <= 0}
+										negative={isUseReward}
+										positive={!isUseReward}
+										onClick={handleUseReward}
+									/>
+								</Grid.Column>
+								<Grid.Column>
+									<PayPalButton
+										new_end={reservation.end_date}
+										new_start={reservation.start_date}
+										user_id={user.uid}
+										total={reservation.price || 100}
+										currency={"USD"}
+										commit={true}
+										onSuccess={onSuccess}
+										onError={onError}
+										onCancel={onCancel}
+									/>
+								</Grid.Column>
+							</Grid.Row>
+						</Grid>
+					</Segment>
+				) : (
+					<div />
+				)}
 			</Modal.Actions>
-			{ isError ? (
+			{isError ? (
 				<Message negative>
 					<Message.Header>Oops!!!</Message.Header>
-					{error.message === "MultipleBookingError" ? <p> Multiple Booking Error: Sorry, Bunker does not allow multiple hotel bookings on concurrent dates!</p> :<p>Something went wrong!</p>}
+					{error.message === "MultipleBookingError" ? (
+						<p>
+							{" "}
+							Multiple Booking Error: Sorry, Bunker does not allow multiple
+							hotel bookings on concurrent dates!
+						</p>
+					) : (
+						<p>Something went wrong!</p>
+					)}
 				</Message>
-			) : null }
-			{ isSuccess ? 			( <Message
-			success
-			header='Your reservation booking was successful!'
-			content='You can view your reservations now at My Reservations'
-			/> ) : null
-
-			}
+			) : null}
+			{isSuccess ? (
+				<Message
+					success
+					header="Your reservation booking was successful!"
+					content="You can view your reservations now at My Reservations"
+				/>
+			) : null}
 		</Modal>
 	);
 };
